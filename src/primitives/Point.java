53a98c16c62a494e9933dd4155c99ca4be631dc5
Yosef Revivo <yosef_revivo@outlook.co.il>
2022-02-28 21:56:31 +0200

    Add javaDoc
    Signed-off-by: Yosef Revivo <yosef_revivo@outlook.co.il>

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,13 +1,27 @@
 package primitives;
 
+/**
+ * Produces a point in the plane that is represented by 3 values and in addition
+ * adds the point to a collector and also subtracts
+ */
 public class Point {
 
     protected final Double3 xyz;
 
+    /**
+     * c'tor Gets a type of type Double3
+     * @param xyz
+     */
     public Point(Double3 xyz) {
         this.xyz = xyz;
     }
 
+    /**
+     * c'tor Gets three values from type double
+     * @param d1
+     * @param d2
+     * @param d3
+     */
     public Point(double d1, double d2, double d3) {
         this(new Double3(d1, d2, d3));
     }
@@ -16,6 +30,11 @@ public class Point {
         return xyz;
     }
 
+    /**
+     * Boolean comparison function between objects
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -30,9 +49,20 @@ public class Point {
         return xyz.toString();
     }
 
+    /**
+     * Vector insert function
+     * @param vector
+     * @return
+     */
     public Point add(Vector vector) {
         return new Point(xyz.add(vector.xyz));
     }
+
+    /**
+     * Vector subtraction function
+     * @param p1
+     * @return
+     */
     public Vector subtract(Point p1) {
         return new Vector(xyz.subtract(p1.xyz));
     }
