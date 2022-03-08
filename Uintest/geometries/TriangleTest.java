6666f064af8c469c3fecfc9446afd153bd0faed5
Yosef Revivo <yosef_revivo@outlook.co.il>
2022-03-08 16:51:12 +0200

    add Tests in geometries and add class TriangleTest

---
+++ Uintest/geometries/TriangleTest.java
@@ -0,0 +1,26 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import geometries.Plane;
+import geometries.Polygon;
+import geometries.Triangle;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+public class TriangleTest {
+
+    @Test
+    void getNormal() {
+// ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple single test here
+        Triangle tr = new Triangle(new Point(0, 0, 1),
+                new Point(1, 0, 0),
+                new Point(0, 1, 0));
+        double sqrt3 = Math.sqrt(1d / 3);
+        assertEquals(new Vector(sqrt3, sqrt3, sqrt3), tr.getNormal(new Point(0, 0, 1)),"Bad normal to triangle");
+    }
+}
