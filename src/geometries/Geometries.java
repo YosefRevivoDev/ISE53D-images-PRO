35bcefc7df22648a9fe86fc9a2b7162b131bb5cb
Yosef Revivo <yosef_revivo@outlook.co.il>
2022-03-23 22:44:46 +0200

    add functions and tests for exercise 3 and create class Geometries

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,62 @@
+package geometries;
+
+import java.util.List;
+
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Geometries is the class representing collection of geometries according to composite pattern
+ * @author Yossef
+ * @author Chaim
+ */
+public class Geometries implements Intersectable {
+
+    private List<Intersectable> _geometries = new LinkedList<>();
+
+    public Geometries(Intersectable... _geometries) {
+        add(_geometries);
+    }
+
+
+    public void Geometries(Intersectable... geometries) {
+        _geometries.addAll(Arrays.asList(geometries));
+    }
+
+    // ***************** Operations ******************** //
+
+    /**
+     * Adds a new shape to the list
+     * @param geometries
+     *               - The shape to add (One of the realists of Geometry)
+     */
+    public void add(Intersectable... geometries) {
+        _geometries.addAll(Arrays.asList(geometries));
+    }
+
+    /**
+     * @param ray Ray -  the ray that intersect the geometries
+     * @return list of Point that intersect the Collection
+     */
+    @Override
+    public List<Point> findIntsersections(Ray ray) {
+
+        if (_geometries.isEmpty()) return null;
+        List<Point> intersections = null;
+
+        for (Intersectable geo : _geometries) {
+            List<Point> tempIntersections = geo.findIntsersections(ray);
+            if (tempIntersections != null) {
+                if (intersections == null)
+                    intersections = new LinkedList<>();
+                intersections.addAll(tempIntersections);
+            }
+        }
+        return intersections;
+    }
+}
